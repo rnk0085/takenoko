c0d1b07528335065a42f637f7f395b3ea5c519eb
rnk0085 <rinawkmbi4k8@gmail.com>
2022-08-20 21:19:43 +0900

    feat: タイマーセット部分だけ作成

--- app/src/main/java/com/rnk0085/android/takenoko/ui/screen/timer/TimerScreen.kt
+++ app/src/main/java/com/rnk0085/android/takenoko/ui/screen/timer/TimerScreen.kt
@@ -1,13 +1,24 @@
 package com.rnk0085.android.takenoko.ui.screen.timer
 
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.rnk0085.android.takenoko.ui.screen.timer.page.TimerSetPage
 import com.rnk0085.android.takenoko.ui.theme.TakenokoTheme
 
 @Composable
-fun TimerScreen() {
-    Text(text = "TIMER")
+fun TimerScreen(
+    modifier: Modifier = Modifier
+) {
+    Box(modifier = modifier.fillMaxSize()) {
+        TimerSetPage(
+            modifier = Modifier.padding(16.dp),
+        )
+    }
 }
 
 @Preview(showBackground = true)
