3f7f9082fde59494b22ac42d08e39a8e7ffae2a2
rnk0085 <rinawkmbi4k8@gmail.com>
2022-08-22 14:01:48 +0900

    feat: 3回振動させる

--- app/src/main/java/com/rnk0085/android/takenoko/MainActivity.kt
+++ app/src/main/java/com/rnk0085/android/takenoko/MainActivity.kt
@@ -22,21 +22,23 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val viewModel: TimerViewModel by viewModels()
+
+        // TODO: 振動する回数を決める
+        val vibrationEffect = VibrationEffect.createWaveform(
+            longArrayOf(1000, 300, 1000, 300, 1000, 300),
+            intArrayOf(VibrationEffect.DEFAULT_AMPLITUDE, 0, VibrationEffect.DEFAULT_AMPLITUDE, 0, VibrationEffect.DEFAULT_AMPLITUDE, 0),
+            -1
+        )
+
         Log.d("debug", "MainActivity")
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
             Log.d("debug", "Build.VERSION_CODES.S")
             val vibratorManager = applicationContext.getSystemService(Context.VIBRATOR_MANAGER_SERVICE) as VibratorManager
-            val vibrationEffect = VibrationEffect.createWaveform(
-                longArrayOf(500L, 500L),
-                intArrayOf(VibrationEffect.DEFAULT_AMPLITUDE, 0),
-                3
-            )
             val combinedVibration = CombinedVibration.createParallel(vibrationEffect)
             vibratorManager.vibrate(combinedVibration)
         } else {
             Log.d("debug", "Not Build.VERSION_CODES.S")
             val vibrator = applicationContext.getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
-            val vibrationEffect = VibrationEffect.createOneShot(5000, VibrationEffect.DEFAULT_AMPLITUDE)
             vibrator.vibrate(vibrationEffect)
         }
 
