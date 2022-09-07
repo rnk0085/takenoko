f1cdf5abeedf3b366295663cf6f253f70bd50ece
rnk0085 <rinawkmbi4k8@gmail.com>
2022-09-08 00:51:37 +0900

    feat: TopPraiseSection を作成

--- app/src/main/java/com/rnk0085/android/takenoko/ui/screen/top/TopScreen.kt
+++ app/src/main/java/com/rnk0085/android/takenoko/ui/screen/top/TopScreen.kt
@@ -1,10 +1,9 @@
 package com.rnk0085.android.takenoko.ui.screen.top
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.Button
 import androidx.compose.material.Text
@@ -13,11 +12,9 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalDensity
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
-import com.rnk0085.android.takenoko.R
+import com.rnk0085.android.takenoko.ui.screen.top.section.TopPraiseSection
 import com.rnk0085.android.takenoko.ui.theme.TakenokoTheme
 
 @Composable
@@ -48,8 +45,10 @@ fun TopScreen(
                 .padding(8.dp)
         ) {
             item {
-                TakenokoCharacterImage(
-                    screenWidth = screenWidth
+                TopPraiseSection(
+                    praiseMessage = uiState.praiseMessage,
+                    screenWidth = screenWidth,
+                    modifier = Modifier.fillMaxWidth().padding(8.dp)
                 )
             }
 
@@ -59,27 +58,6 @@ fun TopScreen(
                 }
             }
         }
-    }
-}
-
-@Composable
-fun TakenokoCharacterImage(
-    screenWidth: Dp
-) {
-    Image(
-        painter = painterResource(id = R.drawable.character2),
-        contentDescription = null,
-        modifier = Modifier.width(screenWidth / 3)
-    )
-}
-
-@Preview
-@Composable
-fun TakenokoCharacterImagePreview() {
-    TakenokoTheme {
-        TakenokoCharacterImage(
-            screenWidth = 200.dp
-        )
     }
 }
 
---
+++ app/src/main/java/com/rnk0085/android/takenoko/ui/screen/top/section/TopPraiseSection.kt
@@ -0,0 +1,62 @@
+package com.rnk0085.android.takenoko.ui.screen.top.section
+
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.rnk0085.android.takenoko.R
+import com.rnk0085.android.takenoko.ui.component.TakenokoCharacterImage
+import com.rnk0085.android.takenoko.ui.theme.TakenokoTheme
+
+@Composable
+fun TopPraiseSection(
+    praiseMessage: String,
+    screenWidth: Dp,
+    modifier: Modifier = Modifier
+) {
+    Row(
+        modifier = modifier,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        // TODO: 定数で定義する
+        Text(
+            text = praiseMessage,
+            modifier = Modifier
+                .border(
+                    width = 2.dp,
+                    color = MaterialTheme.colors.primary,
+                    shape = RoundedCornerShape(8.dp)
+                )
+                .padding(18.dp)
+                .weight(2f),
+            fontSize = 32.sp
+        )
+
+        TakenokoCharacterImage(
+            screenWidth = screenWidth,
+            painter = painterResource(id = R.drawable.character2),
+            modifier = Modifier.weight(1f)
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun TopPraiseSectionPreview() {
+    TakenokoTheme {
+        TopPraiseSection(
+            praiseMessage = "3日連続だね！すごい！！！",
+            screenWidth = 360.dp
+        )
+    }
+}
