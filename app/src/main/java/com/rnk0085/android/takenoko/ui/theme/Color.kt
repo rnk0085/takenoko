90c580cf9ff4d0448790ea6b2b9308ce663dcb1f
rnk0085 <rinawkmbi4k8@gmail.com>
2022-09-09 23:56:06 +0900

    feat: 筍モチーフのキャラのカラーコードを追加

--- app/src/main/java/com/rnk0085/android/takenoko/ui/theme/Color.kt
+++ app/src/main/java/com/rnk0085/android/takenoko/ui/theme/Color.kt
@@ -6,3 +6,11 @@ val Purple200 = Color(0xFFBB86FC)
 val Purple500 = Color(0xFF6200EE)
 val Purple700 = Color(0xFF3700B3)
 val Teal200 = Color(0xFF03DAC5)
+
+// 筍モチーフのキャラで使用しているカラーコード
+val YellowGreen = Color(0xFF8EFF90)
+val EyeBrown = Color(0xFF3F1E1A)
+val CheekPink = Color(0xFFC55E5C)
+val TakenokoInsideBrown = Color(0xFFC58780)
+val TakenokoLineBrown = Color(0xFF8E4B43)
+val TakenokoBottomCream = Color(0xFFFFD699)
