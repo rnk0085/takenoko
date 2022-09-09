0495b07f99109016d35a31cd7e3900c40518de95
rnk0085 <rinawkmbi4k8@gmail.com>
2022-09-10 00:17:12 +0900

    feat: タイマーセット画面のボタンの色を変更

--- app/src/main/java/com/rnk0085/android/takenoko/ui/screen/timer/page/TimerSetPage.kt
+++ app/src/main/java/com/rnk0085/android/takenoko/ui/screen/timer/page/TimerSetPage.kt
@@ -11,7 +11,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
+import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -19,12 +19,14 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.chargemap.compose.numberpicker.FullHours
 import com.chargemap.compose.numberpicker.Hours
 import com.chargemap.compose.numberpicker.HoursNumberPicker
+import com.rnk0085.android.takenoko.ui.theme.TakenokoInsideBrown
 import com.rnk0085.android.takenoko.ui.theme.TakenokoTheme
 import java.time.Duration
 
@@ -53,7 +55,7 @@ fun TimerSetPage(
             // タイマーセット部分
             HoursNumberPicker(
                 modifier = Modifier.padding(horizontal = 8.dp),
-                dividersColor = MaterialTheme.colors.primary,
+                dividersColor = TakenokoInsideBrown,
                 value = pickerValue,
                 onValueChange = {
                     pickerValue = it
@@ -73,13 +75,31 @@ fun TimerSetPage(
                 modifier = modifier.fillMaxWidth(),
                 horizontalArrangement = Arrangement.SpaceEvenly
             ) {
-                Button(onClick = { pickerValue = FullHours(0, 5) }) {
+                Button(
+                    onClick = { pickerValue = FullHours(0, 5) },
+                    colors = ButtonDefaults.buttonColors(
+                        backgroundColor = TakenokoInsideBrown,
+                        contentColor = Color.White
+                    )
+                ) {
                     Text(text = "5分")
                 }
-                Button(onClick = { pickerValue = FullHours(0, 10) }) {
+                Button(
+                    onClick = { pickerValue = FullHours(0, 10) },
+                    colors = ButtonDefaults.buttonColors(
+                        backgroundColor = TakenokoInsideBrown,
+                        contentColor = Color.White
+                    )
+                ) {
                     Text(text = "10分")
                 }
-                Button(onClick = { pickerValue = FullHours(0, 30) }) {
+                Button(
+                    onClick = { pickerValue = FullHours(0, 30) },
+                    colors = ButtonDefaults.buttonColors(
+                        backgroundColor = TakenokoInsideBrown,
+                        contentColor = Color.White
+                    )
+                ) {
                     Text(text = "30分")
                 }
             }
