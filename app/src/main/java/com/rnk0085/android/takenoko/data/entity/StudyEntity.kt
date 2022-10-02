8a45f585b8f02b0761f68efe73188200565f8745
rnk0085 <rinawkmbi4k8@gmail.com>
2022-10-02 11:21:07 +0900

    feat: StudyEntity を作成

---
+++ app/src/main/java/com/rnk0085/android/takenoko/data/entity/StudyEntity.kt
@@ -0,0 +1,21 @@
+package com.rnk0085.android.takenoko.data.entity
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import java.util.*
+
+@Entity(tableName = "study")
+data class StudyEntity(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
+
+    @ColumnInfo(name = "done_message")
+    val doneMessage: String,
+
+    @ColumnInfo(name = "created_at")
+    val createdAt: Date,
+
+    @ColumnInfo(name = "updated_at")
+    val updatedAt: Date
+)
