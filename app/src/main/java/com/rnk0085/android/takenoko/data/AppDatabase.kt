e93ffffdb22689355fe7efda06322f61d904a015
rnk0085 <rinawkmbi4k8@gmail.com>
2022-10-02 12:55:00 +0900

    feat: AppDatabase で DateConverters を使用

--- app/src/main/java/com/rnk0085/android/takenoko/data/AppDatabase.kt
+++ app/src/main/java/com/rnk0085/android/takenoko/data/AppDatabase.kt
@@ -2,13 +2,16 @@ package com.rnk0085.android.takenoko.data
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
 import com.rnk0085.android.takenoko.data.dao.StudyDao
 import com.rnk0085.android.takenoko.data.entity.StudyEntity
+import com.rnk0085.android.takenoko.utils.DateConverters
 
 @Database(
     entities = [StudyEntity::class],
     version = 1
 )
+@TypeConverters(DateConverters::class)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun studyDao(): StudyDao
 }
